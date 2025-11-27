Dr-Zany/rpms-gnome-shell

commit 3fc97daeb220fd69d7613dbd88d11145c6c19a28

Update to 49.0

gnome-shell.spec
@@ -8,7 +8,7 @@
 %endif
 
 Name:           gnome-shell
-Version:        49~rc
+Version:        49.0
 Release:        %autorelease
 Summary:        Window management and application launching for GNOME
 

sources
@@ -1,1 +1,1 @@
-SHA512 (gnome-shell-49.rc.tar.xz) = c9bed8d16767f9fb30455d25a155fd2dba6dd7db85fa33849eba58b991b01278b09f1d016022973727f651cf48efc1200ef59ead0b68bc2a152e0425cdfc66af
+SHA512 (gnome-shell-49.0.tar.xz) = 9af3057ba19796839c7d02e7ee891b6991285504dce98ccf332bf0bf31893b37b3dca0232b7d8f0a70554f54bd6773846448b329e7f03bdf265be06b979407d0

commit 8e6d9a3cbaa5ee8b8c0e4e7977e5d9b12d437a0a

Require GLib 2.86.0
The minimum required GLib version was updated to 2.85.1 and then to
2.86.0 in GNOME Shell 49.beta.1 and 49.0 respectively [1,2].

Fallout from 3fc97daeb220fd69d7613dbd88d11145c6c19a28 and
ad526c6ab22245f748cd70b29ea0207a4de22b3f

[1] GNOME Shell commit c8e28918aa96c533
    https://gitlab.gnome.org/GNOME/gnome-shell/-/commit/c8e28918aa96c533
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3801

[2] GNOME Shell commit 02d9487c1992972a
    https://gitlab.gnome.org/GNOME/gnome-shell/-/commit/02d9487c1992972a
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3860
    https://gitlab.gnome.org/GNOME/glib/-/issues/3776

gnome-shell.spec
@@ -34,7 +34,7 @@ Patch: 3939-rediffed.patch
 
 %define eds_version 3.45.1
 %define gnome_desktop_version 44.0-7
-%define glib2_version 2.79.2
+%define glib2_version 2.86.0
 %define gobject_introspection_version 1.49.1
 %define gjs_version 1.73.1
 %define gtk4_version 4.0.0