Dr-Zany/rpms-gnome-shell

commit 6e304dd7ef75375b7d48f7772e4f0fd2c812d825

Update to 49.beta

gnome-shell.spec
@@ -14,7 +14,7 @@
 %endif
 
 Name:           gnome-shell
-Version:        49~alpha.1
+Version:        49~beta
 Release:        %autorelease
 Summary:        Window management and application launching for GNOME
 
@@ -40,7 +40,7 @@ Patch: 3611.patch
 %define gjs_version 1.73.1
 %define gtk4_version 4.0.0
 %define adwaita_version 1.5.0
-%define mutter_version 48.0
+%define mutter_version 49~beta
 %define polkit_version 0.100
 %define gsettings_desktop_schemas_version 48.0
 %define ibus_version 1.5.2

sources
@@ -1,1 +1,1 @@
-SHA512 (gnome-shell-49.alpha.1.tar.xz) = 5eb16d942e829d7d77525731d79c0b1921a1dfe70f28e019a5d7c79916588bcb899859c479a0f98e36cf7a5c714787511126a1ae128b0c057e3f86077af3668d
+SHA512 (gnome-shell-49.beta.tar.xz) = 000dcf1487454516ac557ba0b304e873abe511ca4cececeaa40e880646f5cfb5917e4212d2037cd74398becab468e5285a59fec6034e48f0990980a0e4d78040

commit f46d9e75a3aa6e495a244dd2be3d21527decc4db

Require gsettings-desktop-schemas 49.alpha
The minimum required gsettings-desktop-schemas version was updated to
49.alpha in GNOME Shell 49.beta [1].

Fallout from 6e304dd7ef75375b7d48f7772e4f0fd2c812d825

[1] GNOME Shell commit 61d5d8f70e28c74a
    https://gitlab.gnome.org/GNOME/gnome-shell/-/commit/61d5d8f70e28c74a
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3797
    https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/1584

gnome-shell.spec
@@ -41,7 +41,7 @@ Patch: 3939-rediffed.patch
 %define adwaita_version 1.5.0
 %define mutter_version 49.0
 %define polkit_version 0.100
-%define gsettings_desktop_schemas_version 48.0
+%define gsettings_desktop_schemas_version 49~alpha
 %define ibus_version 1.5.2
 %define gnome_bluetooth_version 1:42.3
 %define gstreamer_version 1.4.5