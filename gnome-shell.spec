Dr-Zany/rpms-gnome-shell

commit 3fc97daeb220fd69d7613dbd88d11145c6c19a28

Update to 49.0

gnome-shell.spec
@@ -8,7 +8,7 @@
 %endif
 
 Name:           gnome-shell
-Version:        49~rc
+Version:        49.0
 Release:        %autorelease
 Summary:        Window management and application launching for GNOME
 

sources
@@ -1,1 +1,1 @@
-SHA512 (gnome-shell-49.rc.tar.xz) = c9bed8d16767f9fb30455d25a155fd2dba6dd7db85fa33849eba58b991b01278b09f1d016022973727f651cf48efc1200ef59ead0b68bc2a152e0425cdfc66af
+SHA512 (gnome-shell-49.0.tar.xz) = 9af3057ba19796839c7d02e7ee891b6991285504dce98ccf332bf0bf31893b37b3dca0232b7d8f0a70554f54bd6773846448b329e7f03bdf265be06b979407d0

commit 3fe6367a19f00a6e068ed8cb949189909c7d1c53

Require Mutter 49.0
The minimum required Mutter version was updated to 49.rc and then to
49.0 in GNOME Shell 49.rc and 49.0 respectively [1,2].

Fallout from 3fc97daeb220fd69d7613dbd88d11145c6c19a28 and
bfe8115d9c1620e2c8130a22270ae3b259e3f851

[1] GNOME Shell commit fcf3d5389704f9cc
    https://gitlab.gnome.org/GNOME/gnome-shell/-/commit/fcf3d5389704f9cc
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3857

[2] GNOME Shell commit 0632740d08e14aa4
    https://gitlab.gnome.org/GNOME/gnome-shell/-/commit/0632740d08e14aa4
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3883

gnome-shell.spec
@@ -39,7 +39,7 @@ Patch: 3939-rediffed.patch
 %define gjs_version 1.73.1
 %define gtk4_version 4.0.0
 %define adwaita_version 1.5.0
-%define mutter_version 49~beta
+%define mutter_version 49.0
 %define polkit_version 0.100
 %define gsettings_desktop_schemas_version 48.0
 %define ibus_version 1.5.2